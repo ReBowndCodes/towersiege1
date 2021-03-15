aa6f4254b7bdf39302bc5f53350246ca2ff9ca56
ReBowndCodes <73173861+ReBowndCodes@users.noreply.github.com>
2021-03-15 14:06:07 +0530

    Add files via upload

---
+++ boxblue.js
@@ -0,0 +1,13 @@
+class Box extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill("#0756B0");
+    super.display();
+  }
+}
---
+++ boxpink.js
@@ -0,0 +1,14 @@
+class Box1 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+  }
+
+  display()
+  {
+    fill("#C708B7")
+    super.display();
+  }
+}
+     
